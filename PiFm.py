87d6cbfea0276754ed6c26240cfabdb111a5dcfc
Marazair <nick1022m@gmail.com>
2021-09-20 18:11:42 -0400

    feat: add directory handling

--- PiFm.py
+++ PiFm.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
 
-from subprocess import call
+import subprocess
+import os
+import wave
+import ffmpeg
 
 
-def play_sound( filename ):
-   call(["./pifm", filename])
-   return
+def play(path:str, freq = '', sample_rate = '', stereo = '', volume = '') :
+   if os.path.isfile(path):
+      subprocess.call(['./pifm', path, freq, sample_rate, stereo, volume])
+   elif os.path.isdir(path):
+      p = subprocess.Popen(['./pifm', '-', freq, sample_rate, stereo, volume], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+      process_files(path, p)
+   else:
+      raise FileNotFoundError()
+
+def process_files(path:str, p:subprocess.Popen):
+   for _, dirs, files in os.walk(path):
+      for file in [audio_file for audio_file in files if os.path.splitext()[-1] == '.wav']:
+         p.communicate(input=wave.open(file, 'rb'))
+      for dir in dirs:
+         process_files(dir, p)
